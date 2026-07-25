7055974430ea4203dbbc6939bbabae54b61ef0e5
AlaaEid-1 <alaaalaa2382924@gmail.com>
2026-07-25 22:16:22 +0300

    Fix production storage and queue worker setup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -16,5 +16,8 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+echo "Creating storage symlink..."
+php artisan storage:link --force
+
 echo "Starting Supervisor (Nginx + PHP-FPM)..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
